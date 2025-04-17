dea0b75dd327e1b2bfb0d23f6f1a20e7db89bccc
Acer <veronika.vysinova@gmail.com>
2025-04-17 20:52:28 +0200

    Zofka dela ctverce

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,6 +2,8 @@ package cz.czechitas.turtle;
 
 import dev.czechitas.java1.turtle.engine.Turtle;
 
+import java.awt.*;
+
 public class HlavniProgram {
     private Turtle zofka;
 
@@ -11,8 +13,23 @@ public class HlavniProgram {
 
     public void start() {
         zofka = new Turtle();
+spirala (90d,20,10, Color.MAGENTA);
 
-        //TODO Tady bude kód pro kreslení želví grafiky.
     }
+    public void spirala (double uhel,int prirustek,int pocetKroku,Color penColor){
+        zofka.setPenColor(penColor);
+
+        int aktualniDraha = prirustek;
+        for (int i =0; i < pocetKroku; i++) {
+            for(int j =0; j < 4; j++) {
+                zofka.turnRight(uhel);
+                zofka.move(aktualniDraha);
+            }
+
+            aktualniDraha += prirustek;
+        }
+    }
+
+
 
 }
